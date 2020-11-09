f1337d849cb44e8361b239e135971c17ae271ba3
KevinTss <kevin.tassi@gmail.com>
2020-11-09 22:14:14 +0100

    add course

---
+++ serie-1/exercice-1/exercice1.js
@@ -0,0 +1,9 @@
+// 1. Demande une valeur à l’utilisateur, calcule le double de cette valeur et affiche le résultat.
+
+// Demander la valeur
+var a = prompt("Entrez une valeur");
+
+// Calculer le double
+var double = a * 2;
+
+alert("Le double de " + a + " est : " + double);
---
+++ serie-1/exercice-2/exercice2.js
@@ -0,0 +1,26 @@
+// 2. Demande deux valeurs à l’utilisateur, et affiche la somme de celles-ci.
+
+var val1 = Number(prompt("Entrez la valeur 1:"));
+var val2 = Number(prompt("Entrez la valeur 2:"));
+
+var somme = val1 + val2;
+
+alert("La somme de " + val1 + " et " + val2 + " est :" + somme);
+
+// 3. Demande deux valeurs et affiche la moyenne.
+
+// 4. Demande trois valeurs et en affiche la moyenne.
+// 5. Demande les valeurs de x et y, et calcule z tel que y est la moyenne de x et z.
+// 6. Demande les paramètres a et b d’une équation de droite y=ax+b
+// Demande ensuite la valeur d’une abscisse et en calcule l’ordonnée sur la droite
+// 7. Demande les paramètres a et b d’une équation de droite y=ax+b
+// Calcule et affiche son coefficient angulaire, ainsi que les points d’intersection avec les
+// axes X et Y
+// 8. Demande les paramètres a, b et c d’une équation de courbe y=ax2
+// +bx+c
+// Demande ensuite la valeur d’une abscisse et en calcule l’ordonnée sur la courbe
+// 9. Demande les paramètres a, b et c d’une équation du deuxième degré y=ax2
+// +bx+c
+
+// Calcule et affiche son coefficient angulaire, ainsi que les points d’intersection avec les
+// axes X et Y
